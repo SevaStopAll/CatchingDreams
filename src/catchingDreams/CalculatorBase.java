ae035d7579c5016aa3e989ad09018b3bd76cbdc0
SevaStopAll <sevastopallrh@yandex.ru>
2022-10-12 21:29:17 +0300

    First Step
    This is my first step on Github

---
+++ src/catchingDreams/CalculatorBase.java
@@ -0,0 +1,7 @@
+package catchingDreams;
+
+public class CalculatorBase {
+	public static void main(String [] args) {
+		System.out.println("Hello there!");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Guest
+ *
+ */
+module catchingDreams {
+}

443bb947d149b2580987a5438c41476b5c299f91
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-10-17 12:56:54 +0400

    Restored file after my error (wrong merge)

--- src/catchingDreams/CalculatorBase.java
+++ src/catchingDreams/CalculatorBase.java
@@ -1,9 +1,9 @@
 package catchingDreams;
-
+import java.io.*;
 import java.util.Scanner;
 
 public class CalculatorBase {
-	public static void main(String [] args) {
+	public static void main(String [] args) throws Exception {
 		System.out.println("Hello there!");
 		System.out.println("It is a simple calcutalor made just for testing some capacities");
 		Scanner sc = new Scanner(System.in);
@@ -12,48 +12,122 @@ public class CalculatorBase {
 		System.out.println("Please, enter number №2");
 		double num2 = sc.nextDouble();
 		System.out.println("Thank you! Now let's choose the operation!");
-		
+		calc(num1, num2);
+		sc.close();
+
+		sc.close();	
+	}
+
+	public class Counter {
+		  static int operationNumber = 0;
+
+		  public static void invokeCounter() {
+			  operationNumber++;
+		     System.out.println("Operation №" + operationNumber + '\n');
+		  }
+		}
+
+	public static void calc(double num1, double num2) throws Exception {
+
+		int operationNumber = 0;
 		outer :
 			while (true) {
+			System.out.println("0 - Watch the readme");	
 			System.out.println("1 - Addition");
 			System.out.println("2 - Substraction");
 			System.out.println("3 - Multiplication");
 			System.out.println("4 - Division");
 			System.out.println("5 - Remainder of dividing");
-			System.out.println("6- Quit the program");
-		
+			System.out.println("6 - Exponentiation");
+			System.out.println("7 - Quit the program");
+			System.out.println("9 - Choose another numbers");
+			Scanner sc = new Scanner(System.in);
 			int operation = sc.nextInt();
+			FileWriter wr = new FileWriter("MyFile.txt", true);
+			
 			switch (operation) {
+				case (0):
+					System.out.println("This is a pre-pre-pet project, so you can enter 2 numbers by two enters. Hope I can improve it. Thank you");
 				case (1):
 					System.out.println(num1 + num2);
+					wr.write(("Addition result " + (double)(num1 + num2) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " + " + (double) num2 + " = " + (double)(num1 + num2) + '\n'));
+					wr.close();
 					break;
 				case (2):
 					System.out.println(num1 - num2);
+					wr.write(("Substraction result " + (double)(num1 - num2) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " - " + (double) num2 + " = " + (double)(num1 - num2) + '\n'));
+					wr.close();
 					break;
 				case (3):
 					System.out.println(num1 * num2);
+					wr.write(("Multiplication result " + (double)(num1 * num2) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " * " + (double) num2 + " = " + (double)(num1 * num2) + '\n'));
+					wr.close();
 					break;
-				case (4): 
-					if (num2 == 0) {
-						System.out.println("You can't divide by zero. Enter number 2 again!");
-					num2 = sc.nextDouble();
-					}
-					else {
+				case (4): 	
+					try {
+						if(num2 == 0) throw new ArithmeticException();
 					System.out.println(num1 / num2);
-					}
-					break;
+					wr.write(("Division result " + (double)(num1 / num2) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " / " + (double) num2 + " = " + (double)(num1 / num2) + '\n'));
+					wr.close();
+					break; 
+					} catch (ArithmeticException ex){
+						System.out.println("Don't divide by zero.");
+						wr.write( ("Don't divide by zero. " + '\n'));
+						wr.close();
+					}		
 				case (5): {
 					System.out.println(num1 % num2);
+					wr.write(("Remainder of division result " + (double)(num1 % num2) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " % " + (double) num2 + " = " + (double)(num1 % num2) + '\n'));
+					wr.close();
 					break;
 				}	
-				
 				case (6):
+					System.out.println(Math.pow(num1, num2));
+					wr.write(("Exponentiation result " + (double)(Math.pow(num1, num2)) + '\n'));
+					operationNumber++;
+					System.out.println(operationNumber);
+					wr.write("Operation №" + operationNumber + '\n');
+					wr.write(((double) num1 + " ^ " + (double) num2 + " = " + + (double)(Math.pow(num1, num2)) + '\n'));
+					wr.close();
+					break;
+				case (7):
+					System.out.println("Total operations number " + operationNumber);
+					wr.write("You did " + operationNumber + "operations this cycle.");
 					System.out.println("We are closing this app... Bye!");
 					sc.close();
 					break outer;
+				case (9):
+					System.out.println("Please, enter number №1");
+					num1 = sc.nextDouble();
+					System.out.println("Please, enter number №2");
+					num2 = sc.nextDouble();
+					calc(num1, num2);
 				default: 
 					System.out.println("Enter correct number!");
+
+					System.out.println("Enter correct number!");					
 			}
 		}
-	}
 }
+
+}	

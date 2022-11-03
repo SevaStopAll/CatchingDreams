b1414e716344311c5b35a2e1747c109d8838ef60
SevaStopAll <Sevastopallrh@yandex.ru>
2022-11-03 21:43:33 +0400

    add mean methods of booking system
    Now you can:
    1. Watch the flies
    2. Register yourself
    3. Watch the list

--- src/catching/dreams/ticketsbooking/Menu_main.java
+++ src/catching/dreams/ticketsbooking/Menu_main.java
@@ -1,7 +1,22 @@
 package catching.dreams.ticketsbooking;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Menu_main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to CathingDreams Airlines. Soon here will be our service to book your airticket. \n What do we want to realise \n 1.Flights list with destination and time. 2. You can registrate youself and buy a ticket. 3. Our employers will be able to watch passangers list for security reasons.   ");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		processes.Conn();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to CathingDreams Airlines. Soon here will be our service to book your airticket. \nWhat do we want to realise \n1.Flights list with destination and time. 2. You can registrate youself and buy a ticket. \n3. Our employers will be able to watch passangers list for security reasons.");
+		int operation = sc.nextInt();
+		if (operation == 1) {
+			processes.WatchFlightTable();
+		} 
+		else if (operation == 2) {
+			processes.BuyTicket();
+		}
+		else if (operation == 3) {
+			processes.WatchPassangers();
+		}
 	}
 }
--- src/catching/dreams/ticketsbooking/processes.java
+++ src/catching/dreams/ticketsbooking/processes.java
@@ -16,4 +16,41 @@ public static void Conn() throws ClassNotFoundException, SQLException {
 	conn = DriverManager.getConnection("jdbc:sqlite:Booking.db");
 }
 
+public static void WatchFlightTable() throws SQLException {
+	statmt = conn.createStatement();
+	resSet = statmt.executeQuery("Select * from Flights");
+		while(resSet.next()) {
+			int flNumber = resSet.getInt("flight_number");
+			String destination = resSet.getString("flight_destination");
+			int freeSeats = resSet.getInt("free_seats");
+			// Add date 
+			// Add PrintF()
+			System.out.printf("Number %-5s" + " | " + "Destination: %-20s" + " | " + "Free Seats: %-3d\n" , flNumber, destination, freeSeats );
+		}	
+}
+
+public static void BuyTicket() throws SQLException {
+	statmt = conn.createStatement();
+	System.out.println("Please choose the flight!");
+	int flight = sc.nextInt();
+	sc.nextLine();
+	System.out.println("Please enter your name and surname");
+	String nameSurname = sc.nextLine();
+	statmt.execute("INSERT INTO 'Flight_passangers' (Flight, Name_Surname) VALUES ('" + flight + "' , '" + nameSurname+ "' );");
+	resSet = statmt.executeQuery("Select free_seats from 'Flights' WHERE flight_number = '" + flight + "'");
+	int seatsBefore = resSet.getInt("free_seats");
+	int seatsAfter = seatsBefore - 1;
+	statmt.executeUpdate("UPDATE Flights SET free_seats = " + seatsAfter + " WHERE flight_number = '" + flight + "';");		
+}
+
+public static void WatchPassangers() throws SQLException {
+	statmt = conn.createStatement();
+	// Sort by Number than sort by Name;
+	resSet = statmt.executeQuery("Select * from Flight_passangers");
+	while(resSet.next()) {
+		int flNumber = resSet.getInt("Flight");
+		String nameSurname = resSet.getString("Name_Surname");
+		System.out.printf("Number %-5s | Name  %-20s\n", flNumber, nameSurname);
+	}
+}
 }

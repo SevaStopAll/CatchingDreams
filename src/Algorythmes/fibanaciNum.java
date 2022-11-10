dfa0e852f295d5b0b9dc735a2e397427bb2ee7b9
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-11-10 07:57:50 +0400

    delete one algorythme file

--- src/Algorythmes/fibanaciNum.java
+++ src/Algorythmes/fibanaciNum.java
@@ -3,12 +3,21 @@ package Algorythmes;
 
 public class fibanaciNum {
 	public static void main(String[] args) {
-		// int n = 841645;
-		// System.out.println((n));
+		System.out.println(fibNew(20));
 	}
 	
-
+	public static int fibNew(int n) {
+		int [] mas = new int [n+1];
+		mas[0] = 0;
+		mas[1] = 1;
+		for (int i = 2; i <= n; i++) {
+		mas[i] = (mas[i-1] + mas[i-2]);
+		}
+		return mas[n];
+	}
 }
+
+
 
 
 
@@ -27,147 +36,12 @@ public static int fibNew(int n) {
 		mas[0] = 0;
 		mas[1] = 1;
 		for (int i = 2; i <= n; i++) {
-		mas[i] = (mas[i-1] % 10 + mas[i-2] % 10) % 10;
+		mas[i] = (mas[i-1] + mas[i-2]) % 10;
 		}
 		return mas[n];
 	}
 }
 	*/
-
-
-
-/*Третий варинт через  Map и BigInteger 
-
-public class Fibonacci {
-	
-	Map<Integer, BigInteger> cashe = new HashMap<>();
-	
-	private BigInteger fibonacci(int n) {
-		if (n < 2) {
-			return BigInteger.valueOf(n);
-		}
-		if (cashe.containsKey(n)) {
-			return cashe.get(n);
-		} else {
-			BigInteger res = fibonacci(n-1).add(fibonacci(n-2));
-			cashe.put(n, res);
-			return res;
-		}
-	}
-	private void run(int n) {
-		System.out.println(fibonacci(n));
-		
-	}
-	public static void main(String [] args) {
-		long startTime = System.currentTimeMillis();
-		new Fibonacci().run(10);
-		long finishTime = System.currentTimeMillis();
-		System.out.println(startTime - finishTime);
-	}
-}
-*/
-
-
-/* 3 доработанный вариант для уменьшения количества рекурсивных вызовов и без переполнения стека (
-public class Fibonacci {
-	
-	Map<Integer, BigInteger> cashe = new HashMap<>();
-	
-	private BigInteger fibonacci(int n) {
-		if (n < 2) {
-			return BigInteger.valueOf(n);
-		}
-		if (cashe.containsKey(n)) {
-			return cashe.get(n);
-		} else {
-			for (int  i= 2; i <= n; i++) {
-			BigInteger res = fibonacci(i-1).add(fibonacci(i-2));
-			cashe.put(i, res);
-			}
-			return cashe.get(n);
-			
-		}
-	}
-	private void run(int n) {
-		System.out.println(fibonacci(n));
-		
-	}
-	public static void main(String [] args) {
-		long startTime = System.currentTimeMillis();
-		new Fibonacci().run(10);
-		long finishTime = System.currentTimeMillis();
-		System.out.println(startTime - finishTime);
-	}
-}
-
-*/
-
-
-/* 4 доработанный вариант через ArrayList (
-public class Fibonacci {
-
-	private static final int MOD = (int)1e9 + 7;
-	
-	List <Integer> cashe = new ArrayList<>();
-	
-	{
-	cashe.add(0);
-	cashe.add(1);
-	}
-	
-	private int fibonacci(int n) {
-		
-			for (int  i= case.size(); i <= n; i++) {
-				int res = (cashe.get(i - 1) + cashe.get(i - 2)) % MOD;
-				cashe.add(res);
-		}
-		return cashe.get(n)
-	}
-	
-	private void run(int n) {
-		System.out.println(fibonacci(n));
-		
-	}
-	public static void main(String [] args) {
-		long startTime = System.currentTimeMillis();
-		new Fibonacci().run(10);
-		long finishTime = System.currentTimeMillis();
-		System.out.println(startTime - finishTime);
-	}
-}
-
-*/
-
-
-/* 5 доработанный вариант без кэша (
-public class Fibonacci {
-
-	private static final int MOD = (int)1e9 + 7;
-	int a = 0;
-	int b = 1;
-	
-	for (int i = 0; i < n; i++) {
-	int c = (a+b) % MOD;
-	a = b;
-	b = c;
-	}	
- 	return a;
-}
-	
-	private void run(int n) {
-		System.out.println(fibonacci(n));	
-	}
-	public static void main(String [] args) {
-		long startTime = System.currentTimeMillis();
-		new Fibonacci().run(10);
-		long finishTime = System.currentTimeMillis();
-		System.out.println(startTime - finishTime);
-	}
-}
-
-*/
-
-
 
 /* Последняя цифра числа Фибонначи.
 public static int fibNew(int n) {

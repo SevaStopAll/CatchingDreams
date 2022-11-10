8b9cd0d18e4fb680d4cc3249e5b9cd43d888e7f7
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-11-10 11:54:39 +0400

    Add connection to DB

--- src/CinemaFunSite/DB_work.java
+++ src/CinemaFunSite/DB_work.java
@@ -7,17 +7,28 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class DB_Realisation {
+public class DB_work {
 	public static Connection conn;
 	public static Statement statmt;
 	public static ResultSet resSet;
 	public static Scanner sc = new Scanner(System.in);
 	
-	public static void connect() throws SQLException {
-		conn = null;
-		Class.forName("org.sqlite.JDBC");
+	public static void Conn() throws ClassNotFoundException, SQLException 
+	   {
+		   conn = null;
+		   Class.forName("org.sqlite.JDBC");
+		   System.out.println("Please, enter the name of new or existing DB!");
+		   String baseName = sc.nextLine();
 		   conn = DriverManager.getConnection("jdbc:sqlite:cinema.db");
 		   
 		   System.out.println("The base was connected or created!");
-	}
+	   }
+	
+	public static void WriteReview() throws SQLException
+	{
+		statmt = conn.createStatement();
+		String yourReview = sc.nextLine();
+		statmt.execute(query);
+		System.out.println("Done");
+	} 
 }
---
+++ src/CinemaFunSite/MainClass.java
@@ -0,0 +1,11 @@
+package CinemaFunSite;
+
+import java.sql.SQLException;
+
+import dataBasesTraining.conn;
+
+public class MainClass {
+	public static void main(String [] args) throws ClassNotFoundException, SQLException {
+		conn.Conn();
+	}
+}

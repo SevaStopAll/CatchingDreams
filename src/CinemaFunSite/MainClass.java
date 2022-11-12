79abd3e9778cb9d08c26fc00addd7fc40a2ca94a
SevaStopAll <Sevastopallrh@yandex.ru>
2022-11-12 10:49:55 +0400

    delete 4 classes and concentrate all methods in DB_work
    I can't realise separated classes in project. Even with Extends connection with DB loses and provokes critical error.
    If I realise separated methods, I have to change all methods (e.x. changed DB names)

--- src/CinemaFunSite/MainClass.java
+++ src/CinemaFunSite/MainClass.java
@@ -1,12 +1,23 @@
 package CinemaFunSite;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 public class MainClass {
 	public static void main(String [] args) throws ClassNotFoundException, SQLException {
-		DB_Connection.Conn();
-		DB_AddFilm.AddFilm();
-		
+		DB_work.Conn();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to our CinemaReview fun site.");
+		System.out.println("1. Add film  2. Write review  3.Watch reviews");
+		int operation = sc.nextInt();
+		switch (operation) {
+			case(1):
+				DB_work.AddFilm();
+			case(2):
+				DB_work.WriteReview();
+			case(3):
+				DB_work.WatchReview();
+		}
 	}
 }

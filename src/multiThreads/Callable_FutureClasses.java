81ae1a6ecbd41db4b0f11260b6d24054cbebf36c
Vsevolod <115623776+SevaStopAll@users.noreply.github.com>
2022-11-17 07:33:21 +0400

    add new files

--- src/multiThreads/Callable_FutureClasses.java
+++ src/multiThreads/Callable_FutureClasses.java
@@ -10,11 +10,17 @@ public class Callable_FutureClasses {
 		
 		
 		ExecutorService executorService = Executors.newFixedThreadPool(1);
-		executorService.submit(new Callable <Integer>() {
+		executorService.submit(new Callable <Object>() {
 			@Override
-			public Integer call() throws Execption {
-				return null;
-			}
+			public int call() throws Exception {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Finished");
+				return  5;
+				}
 		}); 
 		executorService.shutdown();
 		
